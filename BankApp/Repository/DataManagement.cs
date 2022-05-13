162ae13fb3d9ee909c4e2933cbe010f17fc84a25
SrideviSupriya <supriyams1999@gmail.com>
2022-05-13 13:39:04 +0530

    DockerCompose changes

---
+++ BankApp/Repository/DataManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BankApp.Repository
+{
+    public class DataManagement
+    {
+        //to check if there is migrations which is not implemented in the db to keep the database updated with the latest migration
+        public static void MigrationInitialisation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                // Takes all of our migrations files and apply them against the database in case they are not implemented
+                serviceScope.ServiceProvider.GetService<CustomerDbContext>().Database.Migrate();
+            }
+        }
+    }
+}
--- BankApp/Startup.cs
+++ BankApp/Startup.cs
@@ -65,6 +65,7 @@ namespace BankApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            DataManagement.MigrationInitialisation(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
